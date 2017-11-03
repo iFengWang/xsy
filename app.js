5f7494fd54e3c7c67dee7e1dc2b795fd123b97b7
王广峰 <wanggf@xiaoshouyi.com>
2017-11-03 18:20:16 +0800

    写了一些socket逻辑

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var chat = require('./routes/chat');
 
 var app = express();
+
+app.ready=function(server) {
+  chat.prepareSocketIO(server);
+};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/chat', chat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/chat.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var socketio = require('socket.io');
+
+router.get('/', function(req, res, next) {
+    res.sendFile('/public/index.html');
+});
+
+router.prepareSocketIO = function(server) {
+    var io = socketio.listen(server);
+    io.sockets.on('connection', function(socket) {
+        console.log('有客户端连接啦......');
+    });
+}
+
+module.exports = router;
